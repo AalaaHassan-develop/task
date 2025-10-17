cce400e0a384debd9985c13d3471aa544312a995
alaaHassan-develop <aalaahassan229@gmail.com>
2025-10-17 12:12:43 +0300

    task 3

--- js.js
+++ js.js
@@ -30,3 +30,13 @@ function reverseString(str) {
 }
 
 console.log(reverseString("hello"));
+/*task 3*/
+function factorial(n) {
+    if (n === 0 || n === 1) {
+    return 1;
+    } else {
+    return n * factorial(n - 1);
+    }
+}
+
+console.log(factorial(5));
