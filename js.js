b75f0cbe1c2f0f004bf066320f9346e9a52461d4
alaaHassan-develop <aalaahassan229@gmail.com>
2025-10-17 12:07:58 +0300

    task2

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+/*task 1*/
 function calculator(a,b,operator){
     if(operator=='+'){
         return a+b;
@@ -19,3 +20,13 @@ console.log(calculator(4,2,'+'));
 console.log(calculator(4,2,'-'));
 console.log(calculator(4,2,'*'));
 console.log(calculator(4,2,'/'));
+/*task 2*/
+function reverseString(str) {
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+    }
+    return reversed;
+}
+
+console.log(reverseString("hello"));
