98b4b8fbe238af7d93eeaaa25e4e337797721793
alaaHassan-develop <aalaahassan229@gmail.com>
2025-10-17 12:47:31 +0300

    task 4

--- js.js
+++ js.js
@@ -41,3 +41,14 @@ function factorial(n) {
 
 console.log(factorial(5));
 //task 4
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+    console.log("frizzbuzz");
+    } else if (i % 3 === 0) {
+    console.log("fizz");
+    } else if (i % 5 === 0) {
+    console.log("Buzz");
+    } else {
+    console.log(i);
+    }
+}
